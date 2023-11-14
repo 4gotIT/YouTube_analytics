272fa9aa4e1565d35a2ede707bf8f3c0adef843c
Aziz <ibodov.aziz@yandex.ru>
2023-11-14 22:29:21 +0300

    fix

--- src/chanel.py
+++ src/chanel.py
@@ -7,7 +7,7 @@ class Channel:
     """Класс для ютуб-канала"""
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        self.api_key: str = 'AIzaSyDxVxHK2libki_yT44Wc__Ikjsta-XwnPQ'
+        self.api_key: str = get_key(".env", 'API_KEY')
         self.youtube = build('youtube', 'v3', developerKey=self.api_key)
         self.channel_id = channel_id
         self.channel = self.youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
@@ -15,5 +15,4 @@ class Channel:
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
         print(json.dumps(self.channel, indent=2, ensure_ascii=False))
-
 
