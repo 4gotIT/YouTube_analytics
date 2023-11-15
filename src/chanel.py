de045cdf92d10709c2f70b0ecd818f59aaf6067e
Aziz <ibodov.aziz@yandex.ru>
2023-11-15 12:53:40 +0300

    done 2 task

--- src/chanel.py
+++ src/chanel.py
@@ -4,17 +4,18 @@ from googleapiclient.discovery import build
 
 
 class Channel:
-    """Класс для ютуб-канала"""
+    """Класс аналитика для ютуб-канала"""
+
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        self.channel = self.get_service().channels().list(id=channel_id, part='snippet,statistics').execute()
         self.id = channel_id
-        self.name = self.channel['items'][0]['snippet']['title'] # название канала
-        self.description = self.channel['items'][0]['snippet']['description'] # описание канала
-        self.url = self.channel['items'][0]['snippet']['customUrl'] # ссылка на канал
-        self.subscribercount = self.channel['items'][0]['statistics']['subscriberCount']  # количество подписчиков
-        self.videocount = self.channel['items'][0]['statistics']['videoCount']  # количество видео
-        self.viewcount = self.channel['items'][0]['statistics']['viewCount']  # количество видео
+        self.channel = self.get_service().channels().list(id=channel_id, part='snippet,statistics').execute()
+        self.__title = self.channel['items'][0]['snippet']['title']  # название канала
+        self.__description = self.channel['items'][0]['snippet']['description']  # описание канала
+        self.__url = f'https://www.youtube.com/channel/{self.id}'
+        self.__subscriber_count = self.channel['items'][0]['statistics']['subscriberCount']  # количество подписчиков
+        self.__video_count = self.channel['items'][0]['statistics']['videoCount']  # количество видео
+        self.__view_count = self.channel['items'][0]['statistics']['viewCount']  # количество видео
 
     @classmethod
     def get_service(cls):
@@ -23,13 +24,64 @@ class Channel:
         youtube = build('youtube', 'v3', developerKey=api_key)
         return youtube
 
-    def to_json(self):
-        pass
+    @property
+    def title(self):
+        return self.title
+
+    @property
+    def description(self):
+        return self.description
+
+    @property
+    def url(self):
+        return self.url
+
+    @property
+    def subscriber_count(self):
+        return self.subscriber_count
+
+    @property
+    def video_count(self):
+        return self.video_count
+
+    @property
+    def view_count(self):
+        return self.view_count
+
+    def to_json(self) -> None:
+        attr = {
+            'id': self.id,
+            'name': self.title,
+            'description': self.description,
+            'url': self.url,
+            'subscriber_count': self.subscriber_count,
+            'video_count': self.video_count,
+            'view_count': self.view_count
+        }
+        with open('data.json', 'a', encoding='utf-8') as data:
+            json.dump(attr, data, indent=2, ensure_ascii=False)
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
         print(json.dumps(self.channel, indent=2, ensure_ascii=False))
 
-ex = Channel('UC-OVMPlMA3-YCIeg4z5z23A')
-ex.name = 'fsf'
-print(ex.name)
+
+if __name__ == '__main__':
+
+    moscowpython = Channel('UC-OVMPlMA3-YCIeg4z5z23A')
+
+    # получаем значения атрибутов
+    print(moscowpython.title)  # MoscowPython
+    print(moscowpython.video_count)  # 685 (может уже больше)
+    print(moscowpython.url)  # https://www.youtube.com/channel/UC-OVMPlMA3-YCIeg4z5z23A
+
+    # менять не можем
+    moscowpython.channel_id = 'Новое название'
+    # AttributeError: property 'channel_id' of 'Channel' object has no setter
+
+    # можем получить объект для работы с API вне класса
+    print(Channel.get_service())
+    # <googleapiclient.discovery.Resource object at 0x000002B1E54F9750>
+
+    # создаем файл 'moscowpython.json' в данными по каналу
+    moscowpython.to_json('moscowpython.json')
--- tests/test_chanel.py
+++ tests/test_chanel.py
@@ -2,57 +2,64 @@ from src.chanel import Channel
 
 
 def test_get_info():
-  moscowpython = Channel('UC-OVMPlMA3-YCIeg4z5z23A')
-  assert moscowpython.print_info() == {
-  "kind": "youtube#channelListResponse",
-  "etag": "sq5b4uoagFnb3cyX3DeVlFhXFQ8",
-  "pageInfo": {
-    "totalResults": 1,
-    "resultsPerPage": 5
-  },
-  "items": [
-    {
-      "kind": "youtube#channel",
-      "etag": "oEQuoHPTd2IVnUp5TaD9E0StGBw",
-      "id": "UC-OVMPlMA3-YCIeg4z5z23A",
-      "snippet": {
-        "title": "MoscowPython",
-        "description": "Видеозаписи со встреч питонистов и джангистов в Москве и не только. :)\nПрисоединяйтесь: https://www.facebook.com/groups/MoscowDjango! :)",
-        "customUrl": "@moscowdjangoru",
-        "publishedAt": "2012-07-13T09:48:44Z",
-        "thumbnails": {
-          "default": {
-            "url": "https://yt3.ggpht.com/ytc/APkrFKaVrRJTNkDjSnvpVAYDqbQ5S1VMHWaZhOauk5M10Q=s88-c-k-c0x00ffffff-no-rj",
-            "width": 88,
-            "height": 88
-          },
-          "medium": {
-            "url": "https://yt3.ggpht.com/ytc/APkrFKaVrRJTNkDjSnvpVAYDqbQ5S1VMHWaZhOauk5M10Q=s240-c-k-c0x00ffffff-no-rj",
-            "width": 240,
-            "height": 240
-          },
-          "high": {
-            "url": "https://yt3.ggpht.com/ytc/APkrFKaVrRJTNkDjSnvpVAYDqbQ5S1VMHWaZhOauk5M10Q=s800-c-k-c0x00ffffff-no-rj",
-            "width": 800,
-            "height": 800
-          }
+    moscowpython = Channel('UC-OVMPlMA3-YCIeg4z5z23A')
+    assert moscowpython.print_info() == {
+        "kind": "youtube#channelListResponse",
+        "etag": "sq5b4uoagFnb3cyX3DeVlFhXFQ8",
+        "pageInfo": {
+            "totalResults": 1,
+            "resultsPerPage": 5
         },
-        "localized": {
-          "title": "MoscowPython",
-          "description": "Видеозаписи со встреч питонистов и джангистов в Москве и не только. :)\nПрисоединяйтесь: https://www.facebook.com/groups/MoscowDjango! :)"
-        },
-        "country": "RU"
-      },
-      "statistics": {
-        "viewCount": "2456697",
-        "subscriberCount": "26700",
-        "hiddenSubscriberCount": false,
-        "videoCount": "720"
-      }
+        "items": [
+            {
+                "kind": "youtube#channel",
+                "etag": "oEQuoHPTd2IVnUp5TaD9E0StGBw",
+                "id": "UC-OVMPlMA3-YCIeg4z5z23A",
+                "snippet": {
+                    "title": "MoscowPython",
+                    "description": "Видеозаписи со встреч питонистов и джангистов в Москве и не только. :)\nПрисоединяйтесь: https://www.facebook.com/groups/MoscowDjango! :)",
+                    "customUrl": "@moscowdjangoru",
+                    "publishedAt": "2012-07-13T09:48:44Z",
+                    "thumbnails": {
+                        "default": {
+                            "url": "https://yt3.ggpht.com/ytc/APkrFKaVrRJTNkDjSnvpVAYDqbQ5S1VMHWaZhOauk5M10Q=s88-c-k-c0x00ffffff-no-rj",
+                            "width": 88,
+                            "height": 88
+                        },
+                        "medium": {
+                            "url": "https://yt3.ggpht.com/ytc/APkrFKaVrRJTNkDjSnvpVAYDqbQ5S1VMHWaZhOauk5M10Q=s240-c-k-c0x00ffffff-no-rj",
+                            "width": 240,
+                            "height": 240
+                        },
+                        "high": {
+                            "url": "https://yt3.ggpht.com/ytc/APkrFKaVrRJTNkDjSnvpVAYDqbQ5S1VMHWaZhOauk5M10Q=s800-c-k-c0x00ffffff-no-rj",
+                            "width": 800,
+                            "height": 800
+                        }
+                    },
+                    "localized": {
+                        "title": "MoscowPython",
+                        "description": "Видеозаписи со встреч питонистов и джангистов в Москве и не только. :)\nПрисоединяйтесь: https://www.facebook.com/groups/MoscowDjango! :)"
+                    },
+                    "country": "RU"
+                },
+                "statistics": {
+                    "viewCount": "2456697",
+                    "subscriberCount": "26700",
+                    "hiddenSubscriberCount": false,
+                    "videoCount": "720"
+                }
+            }
+        ]
     }
-  ]
-}
 
 
-if __name__ == '__main__':
-  test_get_info()
+def test_get_attr():
+    moscowpython = Channel('UC-OVMPlMA3-YCIeg4z5z23A')
+
+    # получаем значения атрибутов
+    assert moscowpython.title == 'MoscowPython'
+    assert moscowpython.video_count == 720
+    assert moscowpython.url == 'https://www.youtube.com/channel/UC-OVMPlMA3-YCIeg4z5z23A'
+    assert type(Channel.get_service()) == object
+
