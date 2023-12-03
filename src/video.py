ced2b9d829d403e91b11fe3f429ca4bcbd15d984
Aziz <ibodov.aziz@yandex.ru>
2023-12-03 15:58:45 +0300

    done 6 task

--- src/video.py
+++ src/video.py
@@ -1,7 +1,10 @@
 import json
 from src.chanel import APIMixin
-from dotenv import get_key
-from googleapiclient.discovery import build
+
+
+class Wrong_Id(Exception):
+    def __str__(self):
+        return 'Неверный id_video'
 
 
 class Video(APIMixin):
@@ -13,14 +16,34 @@ class Video(APIMixin):
     def __init__(self, id_video: str):
         self.__id = id_video # id видео
         self.__video = self.get_service().videos().list(id=id_video, part='snippet,statistics').execute()
-        self.__url = f'https://www.youtube.com/watch?v={self.id}' # ссылка на видео
-        self.__title = self.__video["items"][0]["snippet"]["title"] # название видео
-        self.__view_count = self.__video["items"][0]["statistics"]["viewCount"] # количество просмотров
-        self.__likes = self.__video["items"][0]["statistics"]["likeCount"] # количество лайков
+
+        try:
+            self.__url = f'https://www.youtube.com/watch?v={self.id}' # ссылка на видео
+            self.__title = self.__video["items"][0]["snippet"]["title"] # название видео
+            self.__view_count = self.__video["items"][0]["statistics"]["viewCount"] # количество просмотров
+            self.__likes = self.__video["items"][0]["statistics"]["likeCount"] # количество лайков
+
+        except IndexError:
+            self.__url = None
+            self.__title = None
+            self.__view_count = None
+            self.__likes = None
 
     @property
     def id(self):
         return self.__id
+
+    @property
+    def title(self):
+        return self.__title
+
+    @property
+    def view_count(self):
+        return self.__view_count
+
+    @property
+    def like_count(self):
+        return self.__likes
 
     def get_info(self) -> dict:
         '''
@@ -61,10 +84,18 @@ class PLVideo(Video):
         return self.__id_playlist
 
 
-if __name__ == '__main__':
+# if __name__ == '__main__':
     # Создаем два экземпляра класса
-    video1 = Video('AWX4JnAnjBE')  # 'AWX4JnAnjBE' - это id видео из ютуб  RYiwB9OoRvIfowF9
-    video2 = PLVideo('4fObz_qw9u4', 'PLv_zOGKKxVph_8g2Mqc3LMhj0M_BfasbC')
-    assert str(video1) == 'GIL в Python: зачем он нужен и как с этим жить'
-    assert str(video2) == 'MoscowPython Meetup 78 - вступление'
+    # video1 = Video('broken_video_id')  # 'AWX4JnAnjBE' - это id видео из ютуб  RYiwB9OoRvIfowF9
+    # x = Video('zKltRl7uuvM')
+    # video2 = PLVideo('4fObz_qw9u4', 'PLv_zOGKKxVph_8g2Mqc3LMhj0M_BfasbC')
+    # assert str(video1) == 'GIL в Python: зачем он нужен и как с этим жить'
+    # assert str(video2) == 'MoscowPython Meetup 78 - вступление'
+
+if __name__ == '__main__':
+    broken_video = Video('broken_video_id')
+    video1 = Video('AWX4JnAnjBE')
+    print(video1)
+    assert broken_video.title is None
+    assert broken_video.like_count is None
 
